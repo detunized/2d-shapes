9a78e35aed9712d3a0a624d6961c20ac6dd48999
Dmitry Yakimenko <detunized@gmail.com>
2026-02-16 13:08:21 +0100

    Revert "Add varied voice feedback for quiz answers and completion"
    This reverts commit 9a56eede2f9008e527e6fec9b9f89f13966539e4.

--- src/sounds.js
+++ src/sounds.js
@@ -7,27 +7,16 @@ function playAudio(file) {
   a.play();
 }
 
-function pick(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
-const correctPhrases = ['correct', 'good-job', 'well-done', 'nice-one', 'you-got-it'];
-const wrongPhrases = ['not-quite', 'try-again', 'oops', 'almost', 'not-this-time'];
-const completePhrases = ['amazing-work', 'you-did-it', 'great-job', 'fantastic', 'well-played'];
-
 export function playCorrect() {
   playAudio('/audio/correct.mp3');
-  setTimeout(() => playAudio(`/audio/correct-${pick(correctPhrases)}.mp3`), 400);
 }
 
 export function playWrong() {
   playAudio('/audio/wrong.mp3');
-  setTimeout(() => playAudio(`/audio/wrong-${pick(wrongPhrases)}.mp3`), 400);
 }
 
 export function playComplete() {
   playAudio('/audio/complete.mp3');
-  setTimeout(() => playAudio(`/audio/complete-${pick(completePhrases)}.mp3`), 1000);
 }
 
 export function sayShape(name) {
